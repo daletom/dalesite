5259b65e89f1f4884498652f3decde6929785e7f
daletom <tom@imgix.com>
2020-03-20 23:11:08 -0700

    adding amp

--- nuxt.config.js
+++ nuxt.config.js
@@ -45,7 +45,8 @@ export default {
     // Doc: https://bootstrap-vue.js.org
     'bootstrap-vue/nuxt',
     // Doc: https://axios.nuxtjs.org/usage
-    '@nuxtjs/axios'
+    '@nuxtjs/axios',
+    '@nuxtjs/amp'
   ],
   /*
    ** Axios module configuration
